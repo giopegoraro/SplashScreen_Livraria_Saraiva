d6cb3fa6d5b02542a6f79f1294e007f39c9902f5
giopegoraro <giordanopegoraro@gmail.com>
2023-09-01 18:37:28 -0300

    Initial commit

---
+++ app/src/main/java/com/giord/splashscreen/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.giord.splashscreen
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        window.statusBarColor = Color.parseColor("#F7E64F")
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+            finish()
+
+        },3000)
+    }
+}
